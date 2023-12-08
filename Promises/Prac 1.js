0eda5c0d4195ed62c687e363374d9bb0be7d72c6
Amjad Raja <rajaamjad851@gmail.com>
2023-12-08 22:03:36 +0530

    J S Timer Ass completed

---
+++ Promises/Prac 1.js
@@ -0,0 +1,12 @@
+let myPromises1 = new promise(function(resolve, reject){
+    resolve('promise resolved');
+});
+
+let myPromises2 = new Promise(function(resolve, reject){
+    reject('promise rejected');
+});
+
+
+let myPromises3 = new promise(function(resolve, reject){
+
+})
